8770e03914f9bca83de62c87ba866f5929a30080
HYUNGU, KANG <gusrn1423@naver.com>
2019-08-19 19:25:05 +0900

    update - add podspec - update readme.md - fix index.js

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
-import {NativeModules, NativeEventEmitter} from 'react-native';
+import {NativeModules, NativeEventEmitter, Platform} from 'react-native';
 
 const {SpotlightSearch} = NativeModules;
-
 const spotlightEventEmitter = new NativeEventEmitter(SpotlightSearch);
 
-SpotlightSearch.searchItemTapped = (callback) => spotlightEventEmitter.addListener('spotlightSearchItemTapped', callback);
+const EVENT_ITEM_TAPPED = 'spotlightSearchItemTapped';
+const nullFunc = () => {};
 
-export default SpotlightSearch;
+export default {
+    getInitialSearchItem: Platform.select({
+        ios: SpotlightSearch?.getInitialSearchItem,
+        android: nullFunc
+    }),
+    indexItem: Platform.select({
+        ios: (item) => SpotlightSearch?.indexItem(item),
+        android: nullFunc
+    }),
+    indexItems: Platform.select({
+        ios: (items) => SpotlightSearch?.indexItems(items),
+        android: nullFunc
+    }),
+    deleteItemsWithIds: Platform.select({
+        ios: (ids) => SpotlightSearch?.deleteItemsWithIdentifiers(ids),
+        android: nullFunc
+    }),
+    deleteItemsInDomains: Platform.select({
+        ios: (domains) => SpotlightSearch?.deleteItemsInDomains(domains),
+        android: nullFunc
+    }),
+    deleteAllItems: Platform.select({
+       ios: SpotlightSearch?.deleteAllItems,
+       android:  nullFunc
+    }),
+    searchItemTapped: Platform.select({
+        ios: (callback) => spotlightEventEmitter?.addListener(EVENT_ITEM_TAPPED, callback),
+        android: nullFunc
+    }),
+}
---
+++ react-native-spotlight-search.podspec
@@ -0,0 +1,18 @@
+require 'json'
+
+package = JSON.parse(File.read(File.join(__dir__,'package.json')))
+
+Pod::Spec.new do |s|
+  s.name         = package['name']
+  s.version      = package['version']
+  s.summary      = package['description']
+  
+  s.authors      = package['author']
+  s.homepage     = "https://github.com/jdmunro/react-native-spotlight-search#readme"
+  s.license      = package['license']
+  s.platform     = :ios, "9.0"
+
+  s.source       = { :git => "https://github.com/jdmunro/react-native-spotlight-search.git", :tag => "v#{s.version}" }
+  s.source_files  = "ios/**/*.{h,m}"
+  s.dependency 'React'
+end
