9bb6b1e049bf4dacde314b3459e423186c1d8d70
James Munro <me@jdmunro.net>
2020-12-08 18:00:32 +0000

    Add a TypeScript module definition.

---
+++ index.d.ts
@@ -0,0 +1,23 @@
+type SpotlightItem = {
+    title: string
+    contentDescription?: string
+    uniqueIdentifier: string
+    domain: string
+    thumbnailName?: string
+    thumbnailData?: string
+    keywords?: string[]
+}
+
+export function indexItems(items: SpotlightItem[]): Promise<void>
+
+export function indexItem(item: SpotlightItem): Promise<void>
+
+export function deleteItemsWithIdentifiers(itemIdentifiers: string[]): Promise<void>;
+
+export function deleteItemsInDomains(itemDomains: string[]): Promise<void>;
+
+export function deleteAllItems(): Promise<void>
+
+export function searchItemTapped(callback: (uniqueIdentifier: string) => void): void;
+
+export function getInitialSearchItem(): Promise<string>
