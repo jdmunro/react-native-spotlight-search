20eea9e7e098a96b7aa13f6bd38f22befa7454b9
Sakshya Arora <sakshyaarora@Sakshya-Aroras-MacBook-Pro.local>
2021-10-13 22:14:28 +0530

    update plugin code

--- plugin/src/withSpotlight.js
+++ plugin/src/withSpotlight.js
@@ -1,9 +1,43 @@
-import { createRunOncePlugin } from "@expo/config-plugins";
-
+import { createRunOncePlugin, withAppDelegate } from "@expo/config-plugins";
 const pkg = require("react-native-spotlight-search/package.json");
 
-const withSpotlight = (config) => {
-  return config;
+const SPOTLIGHT_IMPORT = `#import "RCTSpotlightSearch.h"`;
+
+const SPOTLIGHT_ACTIVITY = `- (BOOL)application:(UIApplication *)application continueUserActivity:(nonnull NSUserActivity *)userActivity restorationHandler:(nonnull void (^)(NSArray * _Nullable))restorationHandler {
+  [RCTSpotlightSearch handleContinueUserActivity:userActivity];
+  return YES;
+}`;
+
+function modifyAppDelegate(appDelegate) {
+  if (!appDelegate.includes(SPOTLIGHT_IMPORT)) {
+    appDelegate = SPOTLIGHT_IMPORT + appDelegate;
+  }
+  if (appDelegate.includes(SPOTLIGHT_ACTIVITY)) {
+    appDelegate = appDelegate + SPOTLIGHT_ACTIVITY;
+  } else {
+    throw new Error("Failed to detect RCTSpotlightSearch in AppDelegate.m");
+  }
+  return appDelegate;
+}
+
+const withSpotlightAppDelegate = (config) => {
+  return withAppDelegate(config, (config) => {
+    if (config.modResults.language === "objc") {
+      config.modResults.contents = modifyAppDelegate(
+        config.modResults.contents
+      );
+    } else {
+      WarningAggregator.addWarningIOS(
+        "withSpotlightAppDelegate",
+        "Swift AppDelegate files are not supported yet."
+      );
+    }
+    return config;
+  });
 };
 
+const withSpotlight = (config) => {
+  config = withSpotlightAppDelegate(config);
+  return config;
+};
 export default createRunOncePlugin(withSpotlight, pkg.name, pkg.version);
