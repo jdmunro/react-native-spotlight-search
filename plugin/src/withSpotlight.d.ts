60bb718014beee0ceb7d3e91076cf1105739ed46
Sakshya Arora <sakshyaarora@Sakshya-Aroras-MacBook-Pro.local>
2021-10-14 13:56:26 +0530

    change plugin into typescript

---
+++ plugin/src/withSpotlight.d.ts
@@ -0,0 +1,5 @@
+import { ConfigPlugin } from "@expo/config-plugins";
+export declare type Props = {};
+declare const _default: ConfigPlugin<Props>;
+export default _default;
+//# sourceMappingURL=withSpotlight.d.ts.map
